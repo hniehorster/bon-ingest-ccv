63f342155ae85bfe7ad0aa4efb6f7e4f339f24c1
Hjalte Niehorster <h.niehorster@nbproducten.nl>
2022-01-07 21:55:38 +0100

    Fixed the dispatch and endsured that the webhook URLS are secure.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,4 +15,19 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Fix for SwiftMailer Service;
+        $_SERVER["SERVER_NAME"] = "www.salutlabs.com";
+
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+    }
 }
