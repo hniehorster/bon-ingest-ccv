8edf9587700351925f5ec1e882e785ad30835e1b
Hjalte Niehorster <h.niehorster@nbproducten.nl>
2022-02-07 14:49:41 +0100

    Removing the log files.

--- app/Jobs/ProcessOrderJob.php
+++ app/Jobs/ProcessOrderJob.php
@@ -18,7 +18,6 @@ class ProcessOrderJob extends Job implements ShouldQueue
     public $tries = 30;
 
     public $orderData;
-    public $shipmentData;
     public $externalOrderId;
     public $externalIdentifier;
 
@@ -42,8 +41,6 @@ class ProcessOrderJob extends Job implements ShouldQueue
     {
 
         try {
-            Log::info('Processing order: ' . $this->externalOrderId);
-
             $apiCredentials = AuthenticationHelper::getAPICredentials($this->externalIdentifier);
             $webshopAppClient = new WebshopappApiClient($apiCredentials->cluster, $apiCredentials->externalApiKey, $apiCredentials->externalApiSecret, $apiCredentials->language);
 
@@ -64,42 +61,24 @@ class ProcessOrderJob extends Job implements ShouldQueue
                 $bonOrder = $bonApi->orders->create($transformedOrder);
             }
 
-            $this->shipmentData = $webshopAppClient->shipments->get(null, ['order' => $this->externalOrderId]);
-
-            Log::info('Order Products ' . json_encode($this->orderData['products']['resource']['embedded'], JSON_PRETTY_PRINT));
-
             // Let's add the OrderLineItems
             foreach($this->orderData['products']['resource']['embedded'] as $product) {
-
-                Log::info('Products: ' . json_encode($product, JSON_PRETTY_PRINT));
 
                 $transformedLineItem = (new Transformer($apiCredentials->businessUUID, $product, $apiCredentials->defaults))->orderLineItem->transform();
                 $transformedLineItem['order_uuid'] = $bonOrder->uuid;
 
-                Log::info('Transformed LineItem: ' . json_encode($transformedLineItem, JSON_PRETTY_PRINT));
-
                 //Check if Line item already exists
                 $bonLineItemCheck = $bonApi->orderLineItems->get(null, ['order_uuid' => $bonOrder->uuid, 'line_item_id' => $transformedLineItem['line_item_id']]);
 
-                Log::info('Bon Check: ' . json_encode($bonLineItemCheck, JSON_PRETTY_PRINT));
-
                 if($bonLineItemCheck->meta->count > 0) {
-
-                    Log::info('LineItem updated needed');
 
                     $bonLineItem = $bonApi->orderLineItems->update($bonLineItemCheck->data[0]->uuid, $transformedLineItem);
 
-                    Log::info('Updated: ' . json_encode($bonLineItem, JSON_PRETTY_PRINT));
                 }else{
-
-                    Log::info('LineItem create needed');
 
                     $bonLineItem = $bonApi->orderLineItems->create($transformedLineItem);
 
-                    Log::info('Created: ' . json_encode($bonLineItem, JSON_PRETTY_PRINT));
                 }
-
-                Log::info('Line Item Info: ' . json_encode($bonLineItem, JSON_PRETTY_PRINT));
 
                 $shopProduct = $webshopAppClient->products->get($transformedLineItem['product_id']);
 
@@ -111,6 +90,7 @@ class ProcessOrderJob extends Job implements ShouldQueue
             }
         }
         catch (Exception $e) {
+
             if ($e->getCode() == 429) {
                 Queue::later(QueueHelperClass::getNearestTimeRoundedUp(), new ProcessOrderJob($this->externalOrderId, $this->externalIdentifier, $this->orderData));
             }else{
